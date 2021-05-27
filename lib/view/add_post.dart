a884593eca4eec31418047e83285d4a9ce003bb9
qrhfz <mboh.dong@gmail.com>
2021-05-27 18:23:00 +0700

    form add post txt controller

--- lib/view/add_post.dart
+++ lib/view/add_post.dart
@@ -6,12 +6,41 @@ class AddPost extends StatefulWidget {
 }
 
 class _AddPostState extends State<AddPost> {
+  final _titleTxtCtrl = TextEditingController();
+  final _bodyTxtCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Post'),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Title',
+              ),
+              controller: _titleTxtCtrl,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              maxLines: 10,
+              minLines: 4,
+              decoration: InputDecoration(
+                labelText: 'Body',
+                border: OutlineInputBorder(),
+              ),
+              controller: _bodyTxtCtrl,
+            ),
+          ],
+        ),
       ),
     );
   }
